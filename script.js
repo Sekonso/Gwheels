af06baf257fc7ffedbdec19933bca6c6cc4f6bbd
Sekonso <ancaadri9@gmail.com>
2023-12-11 19:46:46 +0700

    Patch: history, collection, gallery

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ navToggle.forEach((toggle) => {
     navList.classList.toggle("inactive");
   });
 });
+
+const cards = document.querySelectorAll(".collection-cards .card");
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    const activeCard = document.querySelector(".collection-cards .card.active");
+    if (activeCard && activeCard !== card) {
+      activeCard.classList.remove("active"); // Corrected from activePanel to activeCard
+    }
+    card.classList.add("active"); // Corrected from panel to card
+  });
+});
